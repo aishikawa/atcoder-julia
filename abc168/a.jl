b1242f86d99c9d6e9a4d2c1d0a8378f3869eba0b
A Ishikawa <a.ishikawa3@gmail.com>
2020-09-25 20:53:02 +0900

    Solve ABC 168 problem A

---
+++ abc168/a.jl
@@ -0,0 +1,12 @@
+function solve()
+    n = readline()
+    if n[end] in ['2', '4', '5', '7', '9']
+        "hon"
+    elseif n[end] in ['0', '1', '6', '8']
+        "pon"
+    else
+        "bon"
+    end
+end
+
+println(solve())
