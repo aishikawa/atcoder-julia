94c0ff244a19ae8e190575ac3e85b621a687c8b0
aishikawa <a.ishikawa3@gmail.com>
2026-08-15 22:42:36 +0900

    Refactor ABC 163 c.jl

--- abc161-170/abc163/c.jl
+++ abc161-170/abc163/c.jl
@@ -1,10 +1,10 @@
 function solve()
     n = parse(Int, readline())
-    a = [parse(Int, x) for x in split(readline())]
+    A = [parse(Int, x) for x in split(readline())]
 
     ans = zeros(Int, n)
-    for i=1:length(a)
-        ans[a[i]] += 1
+    for a in A
+        ans[a] += 1
     end
 
     for x in ans
